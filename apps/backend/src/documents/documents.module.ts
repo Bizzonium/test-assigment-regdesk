70162ad91bbc146298e5b240b980984b406c9ee6
Bizzonium <azizonkg@gmail.com>
2023-10-29 03:34:13 +0600

    style: format to comply linter

--- apps/backend/src/documents/documents.module.ts
+++ apps/backend/src/documents/documents.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
+import { DocumentsController } from './documents.controller'
+import { DocumentsService } from './documents.service'
 import { Document, DocumentSchema } from './schemas/document.schema'
-import { DocumentsController } from './documents.controller';
-import { DocumentsService } from './documents.service';
 
 @Module({
   imports: [
--- apps/backend/src/fields/fields.module.ts
+++ apps/backend/src/fields/fields.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
 import { FieldType } from './enums/field-type.enum'
+import { FieldsController } from './fields.controller'
+import { FieldsService } from './fields.service'
 import { FieldContainerSchema } from './schemas/field-container.schema'
 import { FieldTextInputSchema } from './schemas/field-text-input.schema'
 import { Field, FieldSchema } from './schemas/field.schema'
-import { FieldsController } from './fields.controller';
-import { FieldsService } from './fields.service';
 
 @Module({
   imports: [
